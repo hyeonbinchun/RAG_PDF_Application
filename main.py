f97c2c5f048173a9cd1cdddf4ec024f5842abdf2
owen <owen2781379@gmail.com>
2026-05-21 16:07:54 -0400

    [ADD] Vector Database Setup

--- main.py
+++ main.py
@@ -21,10 +21,12 @@ inngest_client = inngest.Inngest(
     fn_id="RAG: Ingest PDF",   
     trigger=inngest.TriggerEvent(event="rag/ingest_pdf"),
 )
+
 async def rag_ingest_pdf(ctx: inngest.Context):
     return {"hello": "world"}
 
 
 app = FastAPI()
+
 
 inngest.fast_api.serve(app, inngest_client, [rag_ingest_pdf])
